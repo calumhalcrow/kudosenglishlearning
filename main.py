6947aa822fd28d5b9e8e6c53ca5828d8a78c36fc
Calum Halcrow <calumhalcrow@gmail.com>
2011-05-30 14:56:41 +0800

    Can handle page URLs.
    Also added About page shell.

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 import os
 from google.appengine.ext.webapp import template
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
+class BrowseHandler(webapp.RequestHandler):
+    def get(self, page_name):
+        path = os.path.join(os.path.dirname(__file__), 'templates/'+page_name+'.html')
         self.response.out.write(template.render(path, {}))
 
 application = webapp.WSGIApplication(
-    [('/', MainPage)],
+    [(r'^/(.*)/?$', BrowseHandler)],
     debug=True)
 
 def main():
