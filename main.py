f76fb260aad17162958426810eedbf39e43db800
Calum Halcrow <calumhalcrow@gmail.com>
2011-06-19 22:34:35 +0800

    Highlight current page in nav.
    This involves pulling nav items out of base.html, instead
    passing the items to the template via a dictionary. From this
    it's quite convenient to add a "current" class to nav items
    where necessary.

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ from google.appengine.ext.webapp import template
 
 class BrowseHandler(webapp.RequestHandler):
     def get(self, page_name):
+        nav = {
+                '': 'Home',
+                'about': 'About',
+                'trainers-profile': "Trainer's Profile",
+                'training-programmes': 'Training Programmes',
+                'photos': 'Photos',
+                'testimonials': 'Testimonials',
+        }
         template_name = 'templates/'+page_name+'.html' if page_name else 'templates/index.html'
         path = os.path.join(os.path.dirname(__file__), template_name)
-        self.response.out.write(template.render(path, {}))
+        self.response.out.write(
+            template.render(path, {'current': page_name, 'nav': nav }))
 
 application = webapp.WSGIApplication(
     [(r'^/(.*)/?$', BrowseHandler)],
