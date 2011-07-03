eaa8c91ab1553311ecab35ec23f004a350fe7c4f
Calum Halcrow <calumhalcrow@gmail.com>
2011-07-03 22:21:45 +0800

    Introduce JQuery, and contact form event handlers.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+    var contact_form = $('#contact-form');
+
+    // contact_form.hide();
+
+    $('#show-contact').click( function() {
+        contact_form.show('slow');
+        return false;
+    });
+
+    $('#hide-contact').click( function() {
+        contact_form.hide('slow');
+        return false;
+    });
+});
