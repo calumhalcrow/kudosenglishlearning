5a93c8c803cf5aa9de05e7128e455bf2ec874e41
Calum Halcrow <calumhalcrow@gmail.com>
2011-07-20 20:04:32 +0800

    Adds client-side checks to contact form.
    Server-side validation and processing up next...

--- js/main.js
+++ js/main.js
@@ -15,4 +15,32 @@ $(function() {
         contact_form.hide(1000);
         return false;
     });
+
+    $('#contact-form').submit( function() {
+
+        // client-side checks
+        var form = $(this);
+        form.find('#name, #email, #message').each(function() {
+            var elt = $(this);
+            if (!elt.val()) {
+                elt.addClass('empty');
+            }
+            else {
+                elt.removeClass('empty');
+            }
+        });
+        if (form.find('.empty').index() != -1)
+        {
+            if (form.find('#required_error').index() == -1)
+            {
+                form.find('p:first').append(
+                    $('<p>')
+                    .attr('id', 'required_error')
+                    .text('Some required fields have not been completed.')
+                );
+            }
+        }
+
+        return false;
+    });
 });
